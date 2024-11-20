c6559fa4242ee04922d47a89ea446cee2dc4e2a7
Joao Vitor Diamon <joao.santos2112@etec.sp.gov.br>
2024-11-19 21:29:38 -0300

    feat: Configurando Vercelssssssssssssssssss toma rweiug dbsujialfhgvuiyewoASHUDUIOSAHDUIAS

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schemas/index'
-import { env } from './http/env'
 
-export const client = postgres(env.DATABASE_URL)
+export const client = postgres("postgresql://EventEasy_owner:ZDoJGPY5WH9k@ep-summer-mud-a5umaof9.us-east-2.aws.neon.tech/EventEasy?sslmode=require")
 export const db = drizzle(client, { schema, logger: true },)
 
 
--- src/http/env.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-const envSchema = z.object({
-   DATABASE_URL: z.string().url(),
-});
-
-
-
-export const env = envSchema.parse("postgresql://EventEasy_owner:ZDoJGPY5WH9k@ep-summer-mud-a5umaof9.us-east-2.aws.neon.tech/EventEasy?sslmode=require");
