8771c3cc04d550577615fa25fdc371300fbeb5bc
Joao Vitor Diamon <joao.santos2112@etec.sp.gov.br>
2024-11-19 21:21:43 -0300

    feat: Configurando Vercelssssssssssssssssss

--- src/http/env.ts
+++ src/http/env.ts
@@ -1,9 +1,11 @@
-import z from 'zod'
-
+import z from 'zod';
 
 const envSchema = z.object({
-   DATABASE_URL:z.string().url()
-   
-})
+  DATABASE_URL: z.string().url(),
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing in environment variables");
+}
 
 export const env = envSchema.parse(process.env);
