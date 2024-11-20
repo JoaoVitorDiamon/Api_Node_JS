9f3f6e9e44fd6d8b6f3ffb85172de84310882aea
Joao Vitor Diamon <joao.santos2112@etec.sp.gov.br>
2024-11-19 22:13:07 -0300

    me ajuda jesusd

--- src/api/http/server.ts
+++ src/api/http/server.ts
@@ -1,63 +1,47 @@
-import fastify from "fastify";
-import { getEvents } from "./routes/Events/get-all-events-route";
-import {
-	serializerCompiler,
-	validatorCompiler,
-	type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { getGuests } from "./routes/Guests/get-all-guests-route";
-import { createsGuest } from "./routes/Guests/create-guests-route";
-import { updatesGuests } from "./routes/Guests/update-guest-route";
-import { DeleteGuests } from "./routes/Guests/delete-guest-route";
-import { getKittys } from "./routes/Kitty/get-all-kittys-route";
-import { createKittys } from "./routes/Kitty/create-kitty-route";
-import { updatesKitty } from "./routes/Kitty/update-kitty-route";
-import { DeleteKitty } from "./routes/Kitty/delete-kitty-router";
-import { createEvent } from "./routes/Events/create-event-route";
-import { DeleteEvent } from "./routes/Events/delete-events-route";
-import { updateEvent } from "./routes/Events/update-event-route";
-import { getBuyLists } from "./routes/BuyList/get-all-buyList-route";
-import { createBuyLists } from "./routes/BuyList/create-buyList-route";
-import { updateBuyLists } from "./routes/BuyList/update-buyList-route";
-import { deleteBuyLists } from "./routes/BuyList/delete-buyList";
-import { getEventsAndGuests } from "./routes/EventAndGuests/get-all-event-and-guests";
-import { getTokenSpotifyy } from "./routes/SpotifyToken/token_permission";
-import { getEventsByID } from "./routes/Events/find-by-id-event-route";
-import { getAdress } from "./routes/Adress/get-all-adress-route";
-import { createdAdress } from "./routes/Adress/create-adress-router";
-import { updatesAdress } from "./routes/Adress/update-adress-route";
-import deletesAdress from "./routes/Adress/delete-adress-route";
-import { getGuestsByID } from "./routes/Guests/find-by-id-guests-route";
-import { getAdressByID } from "./routes/Adress/find-by-id-adress-route";
-import cors from "@fastify/cors";
-import { getEventsByUserID } from "./routes/Events/find-by-userID-events";
+import fastify from 'fastify';
+import { getEvents } from './routes/Events/get-all-events-route';
+import { getGuests } from './routes/Guests/get-all-guests-route';
+import { createsGuest } from './routes/Guests/create-guests-route';
+import { updatesGuests } from './routes/Guests/update-guest-route';
+import { DeleteGuests } from './routes/Guests/delete-guest-route';
+import { getKittys } from './routes/Kitty/get-all-kittys-route';
+import { createKittys } from './routes/Kitty/create-kitty-route';
+import { updatesKitty } from './routes/Kitty/update-kitty-route';
+import { DeleteKitty } from './routes/Kitty/delete-kitty-router';
+import { createEvent } from './routes/Events/create-event-route';
+import { DeleteEvent } from './routes/Events/delete-events-route';
+import { updateEvent } from './routes/Events/update-event-route';
+import { getBuyLists } from './routes/BuyList/get-all-buyList-route';
+import { createBuyLists } from './routes/BuyList/create-buyList-route';
+import { updateBuyLists } from './routes/BuyList/update-buyList-route';
+import { deleteBuyLists } from './routes/BuyList/delete-buyList';
+import { getEventsAndGuests } from './routes/EventAndGuests/get-all-event-and-guests';
+import { getTokenSpotifyy } from './routes/SpotifyToken/token_permission';
+import { getEventsByID } from './routes/Events/find-by-id-event-route';
+import { getAdress } from './routes/Adress/get-all-adress-route';
+import { createdAdress } from './routes/Adress/create-adress-router';
+import { updatesAdress } from './routes/Adress/update-adress-route';
+import deletesAdress from './routes/Adress/delete-adress-route';
+import { getGuestsByID } from './routes/Guests/find-by-id-guests-route';
+import { getAdressByID } from './routes/Adress/find-by-id-adress-route';
+import cors from '@fastify/cors';
+import { getEventsByUserID } from './routes/Events/find-by-userID-events';
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
 
 const app = fastify({
-	requestTimeout: 30000,
+  requestTimeout: 30000,
 }).withTypeProvider<ZodTypeProvider>();
+
 app.register(cors, {
-	origin: '*',
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 });
-
-
-if (!process.env.VERCEL) { 
-  app.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
-    if (err) {
-      console.error('Erro ao iniciar o servidor:', err);
-      process.exit(1);
-    }
-    console.log(`Servidor HTTP rodando em ${address}`);
-  });
-} else {
-  module.exports = app;
-}
-
 
 app.get('/', async (request, reply) => {
   return { hello: 'Mundo e Lindo' };
 });
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
@@ -67,7 +51,7 @@ app.register(createEvent);
 app.register(updateEvent);
 app.register(DeleteEvent);
 app.register(getEventsByID);
-app.register(getEventsByUserID)
+app.register(getEventsByUserID);
 
 // Guests / Convidados
 app.register(getGuests);
@@ -88,7 +72,8 @@ app.register(createdAdress);
 app.register(updatesAdress);
 app.register(deletesAdress);
 app.register(getAdressByID);
-//BuyList / Lista de Compras
+
+// BuyList / Lista de Compras
 app.register(getBuyLists);
 app.register(createBuyLists);
 app.register(updateBuyLists);
@@ -97,6 +82,19 @@ app.register(deleteBuyLists);
 // EventAndGuests / Eventos e Convidados
 app.register(getEventsAndGuests);
 
-//Spotify
+// Spotify
 app.register(getTokenSpotifyy);
-export default app; 
+
+// Para execução local, adicione um `listen` padrão
+if (process.env.NODE_ENV !== 'production') {
+  const port = 3333;
+  app.listen({ port, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      console.error('Erro ao iniciar o servidor:', err);
+      process.exit(1);
+    }
+    console.log(`Servidor HTTP rodando em ${address}`);
+  });
+}
+
+export default app;
