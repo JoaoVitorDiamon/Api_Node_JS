15d903eeeec3c85b527f3ed9dfebb09c98512312
Joao Vitor Diamon <joao.santos2112@etec.sp.gov.br>
2024-11-19 21:56:30 -0300

    versao do karalfasdasdas

--- src/api/http/routes/Adress/update-adress-route.ts
+++ src/api/http/routes/Adress/update-adress-route.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import app  from "../../server";
+import app from "../../server";
 import { updateAdress } from "../../../../functions/Adress/update-all-adress";
 
 export const updatesAdress = async () => {
@@ -17,6 +17,7 @@ export const updatesAdress = async () => {
 					city: z.string({ message: "city is required" }),
 					state: z.string({ message: "state is required" }),
 					complement: z.string({ message: "complement is required" }),
+					userID: z.string({ message: "userID is required" }),
 					neighborhood: z.string({ message: "neighborhood is required" }),
 					country: z.string({ message: "country is required" }),
 				}),
@@ -31,18 +32,19 @@ export const updatesAdress = async () => {
 				city,
 				state,
 				complement,
+				userID,
 				neighborhood,
 				country,
 			} = request.body;
 			const updatedAdress = await updateAdress(
-				{ cep, street, number, city, state, complement, neighborhood, country },
+				{ cep, street, number, city, state, complement, neighborhood, country, userID },
 				id,
 			);
 
-      reply.status(204).send({
-        message: "Endereco atualizado com sucesso",
-        data: updatedAdress,
-      });
+			reply.status(204).send({
+				message: "Endereco atualizado com sucesso",
+				data: updatedAdress,
+			});
 		},
 	);
 };
