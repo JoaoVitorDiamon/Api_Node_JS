791e24b50bd25da6bdd757d8f5a8fd604098a9d1
Joao Vitor Diamon <joao.santos2112@etec.sp.gov.br>
2024-11-19 23:07:01 -0300

    me ajuda jesus

--- src/api/http/server.ts
+++ src/api/http/server.ts
@@ -18,21 +18,28 @@ import fastify from 'fastify';
 // import { getEventsAndGuests } from './routes/EventAndGuests/get-all-event-and-guests';
 // import { getTokenSpotifyy } from './routes/SpotifyToken/token_permission';
 // import { getEventsByID } from './routes/Events/find-by-id-event-route';
-import { getAdress } from './routes/Adress/get-all-adress-route';
-import { createdAdress } from './routes/Adress/create-adress-router';
-import { updatesAdress } from './routes/Adress/update-adress-route';
-import deletesAdress from './routes/Adress/delete-adress-route';
+// import { getAdress } from './routes/Adress/get-all-adress-route';
+// import { createdAdress } from './routes/Adress/create-adress-router';
+// import { updatesAdress } from './routes/Adress/update-adress-route';
+// import deletesAdress from './routes/Adress/delete-adress-route';
 // import { getGuestsByID } from './routes/Guests/find-by-id-guests-route';
-import { getAdressByID } from './routes/Adress/find-by-id-adress-route';
+// import { getAdressByID } from './routes/Adress/find-by-id-adress-route';
 import cors from '@fastify/cors';
 // import { getEventsByUserID } from './routes/Events/find-by-userID-events';
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
+import { serializerCompiler, validatorCompiler, type FastifyPluginAsyncZod, type ZodTypeProvider } from 'fastify-type-provider-zod';
 // import { getAllAdress } from '../../functions/Adress/get-all-adress';
-import type { FastifyRequest, FastifyReply } from 'fastify';
+import type { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify';
+import { getAllAdress } from '../../functions/Adress/get-all-adress';
+import { z } from 'zod';
+import { createAdress } from '../../functions/Adress/create-adress';
+
+
+
+
 
 const app = fastify({
 	requestTimeout: 30000,
-}).withTypeProvider<ZodTypeProvider>(); 
+}).withTypeProvider<ZodTypeProvider>();
 app.register(cors, {
 	origin: '*',
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -43,6 +50,60 @@ app.get('/', async (request, reply) => {
 	return { hello: 'Mundo e Lindo' };
 });
 
+app.get("/adress", async (_req, reply) => {
+	const getAllAdresss = await getAllAdress();
+	reply.status(200).send(getAllAdresss);
+});
+const AddressSchema = z.object({
+	cep: z.string({ message: "cep is required" }),
+	street: z.string({ message: "street is required" }),
+	number: z.number({ message: "number is required" }),
+	city: z.string({ message: "city is required" }),
+	state: z.string({ message: "state is required" }),
+	userID: z.string({ message: "userID is required" }),
+	complement: z.string({ message: "complement is required" }),
+	neighborhood: z.string({ message: "neighborhood is required" }),
+	country: z.string({ message: "country is required" }),
+});
+
+
+
+
+app.post(
+	"/adress",
+	{
+		schema: {
+			body: AddressSchema,
+		},
+	},
+	async (req, reply) => {
+		const {
+			cep,
+			street,
+			number,
+			city,
+			state,
+			complement,
+			userID,
+			neighborhood,
+			country,
+		} = req.body as z.infer<typeof AddressSchema>;
+		const createdAdress = await createAdress({
+			cep,
+			street,
+			number,
+			city,
+			state,
+			userID,
+			complement,
+			neighborhood,
+			country,
+		});
+		reply
+			.status(201)
+			.send({ message: "Adress created successfully", id: createdAdress });
+	},
+);
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -62,11 +123,11 @@ app.setSerializerCompiler(serializerCompiler);
 // app.register(createKittys);
 // app.register(updatesKitty);
 // app.register(DeleteKitty);
-app.register(getAdress);
-app.register(createdAdress);
-app.register(updatesAdress);
-app.register(deletesAdress);
-app.register(getAdressByID);
+// app.register(getAdress);
+// app.register(createdAdress);
+// app.register(updatesAdress);
+// app.register(deletesAdress);
+// app.register(getAdressByID);
 // app.register(getBuyLists);
 // app.register(createBuyLists);
 // app.register(updateBuyLists);
